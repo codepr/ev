6b2690c65fdec7a6fa61f5260ec797e768d47b5d
Andrea <a.g.baldan@gmail.com>
2020-03-21 11:33:23 +0100

    Add some error cases on example

--- examples/ev_tcp_server.c
+++ examples/ev_tcp_server.c
@@ -8,12 +8,20 @@ static void on_data(ev_tcp_client *client) {
     if (strncmp(client->buf, "quit", 4) == 0)
         ev_tcp_close_connection(client);
     else
-        ev_tcp_write(client);
+        (void) ev_tcp_write(client);
 }
 
 static void on_connection(ev_tcp_server *server) {
+    int err = 0;
     ev_tcp_client *client = malloc(sizeof(*client));
-    ev_tcp_server_accept(server, client, on_data);
+    if ((err = ev_tcp_server_accept(server, client, on_data)) < 0) {
+        if (err < 0) {
+            if (err == -1)
+                fprintf(stderr, "Something went wrong %s\n", strerror(errno));
+            else
+                fprintf(stderr, "Something went wrong %s\n", ev_tcp_err(err));
+        }
+    }
 }
 
 int main(void) {
@@ -21,7 +29,13 @@ int main(void) {
     ev_context *ctx = ev_get_ev_context();
     ev_tcp_server server;
     ev_tcp_server_init(&server, ctx, 128);
-    ev_tcp_server_listen(&server, "127.0.0.1", 5959, on_connection);
+    int err = ev_tcp_server_listen(&server, "127.0.0.1", 5959, on_connection);
+    if (err < 0) {
+        if (err == -1)
+            fprintf(stderr, "Something went wrong %s\n", strerror(errno));
+        else
+            fprintf(stderr, "Something went wrong %s\n", ev_tcp_err(err));
+    }
     ev_tcp_server_stop(&server);
 
     return 0;
